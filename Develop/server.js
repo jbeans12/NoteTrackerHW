9391d4a86b3ae9b55f65435ee0f737a7743d4610
Jennifer Vereecken <77600929+jbeans12@users.noreply.github.com>
2021-03-17 21:40:54 -0700

    got new notes to populate

--- Develop/server.js
+++ Develop/server.js
@@ -1,19 +1,30 @@
 const express = require('express'); 
 const path = require('path');
+const fs = require('fs');
+const noteDB = JSON.parse(fs.readFileSync('./db/db.json'));
+// const { v4: uuidv4 } = require("uuid");
+const { response } = require('express');
+
 
 const app = express();
 const PORT = process.env.PORT || 6500;
 
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+//returns index.html
+app.get('/', (req,res) => res.sendFile(path.join(__dirname, './public/index.html')));
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
+//returns notes.html
+app.get('/notes', (req,res) => res.sendFile(path.join(__dirname, './public/notes.html')));
 
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
-
-app.get('/api/tables', (req, res) => res.json(tables));
-
-
+app.get('/api/notes', (req,res) => res.json(noteDB));
+app.post('/api/notes', (req, res) => {
+    const newNote = req.body
+    newNote.id = req.body.title
+    noteDB.push(newNote)
+    response.json(newNote)
+});
 
 app.listen(PORT, () => console.log(`app listening on PORT ${PORT}`));
