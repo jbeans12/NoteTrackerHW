f396484538f42a76200696876b7c75ece250bde6
Jennifer Vereecken <77600929+jbeans12@users.noreply.github.com>
2021-03-17 19:37:15 -0700

    server.js update

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,19 @@
 const express = require('express'); 
+const path = require('path');
+
 const app = express();
 const PORT = process.env.PORT || 6500;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
+
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
+
+app.get('/api/tables', (req, res) => res.json(tables));
+
 
 
 app.listen(PORT, () => console.log(`app listening on PORT ${PORT}`));
